2041c0bcc66233996dee61df99db9cbfed4bde85
david <dlui93@yahoo.com>
2021-10-26 17:16:52 -0400

    Updates

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style = "width: 100%; opacity: 85%; text-align: center;";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.body.style.backgroundColor = "white";
-}
